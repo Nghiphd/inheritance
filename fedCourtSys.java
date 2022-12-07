09098d859234d158c68ca85d9a9fd9edd73b2453
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-12-07 11:41:08 -0800

    meh

--- fedCourtSys.java
+++ fedCourtSys.java
@@ -11,14 +11,31 @@ public class fedCourtSys
     private boolean fedLaw;
     private int damages;
     private boolean diffStates;
-    public fedCourtSys(int damages, boolean diffStates, boolean fedLaw,
-    boolean bankruptcy)
+    public fedCourtSys(int damage, boolean States, boolean fed,
+    boolean bank)
     {
-        this.damages = damages;
+        this.damages = damage;
+        this.diffStates = States;
+        this.fedLaw = fed;
+        this.bankruptcy = bank;
     }
     public boolean accepted()
     {
         return (damages > 75000 && diffStates || bankruptcy || fedLaw);
     }
-    System.out.println(this.damages[] + diffStates + fedLaw);
+    public int constitution(int constitution){
+        return constitution;
+    }
+    public void bankruptcy(boolean bankruptcy) {
+        this.bankruptcy = bankruptcy;
+    }
+    public void fedlaw(boolean fedLaw) {
+        this.fedLaw = fedLaw;
+    }
+    public void diffStates(boolean diffStates) {
+        this.diffStates = diffStates;
+    }
+    public void damages(int damages) {
+        this.damages = damages;
+    }
 }
---
+++ uSSupremeCourt.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class uSSupremeCourt here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class uSSupremeCourt extends fedCourtSys
+{
+    public uSSupremeCourt(int damage, boolean States, boolean fed,
+    boolean bank) {
+        super(damage,States,fed,bank);
+    }
+    public void procedee() {
+        if(accepted()==true) {
+            
+        }
+    }
+}
